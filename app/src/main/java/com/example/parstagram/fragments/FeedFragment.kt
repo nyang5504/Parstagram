cb0d26ed730d397eba72ae663870ef66231f246d
nyang5504 <ninayang3.14@gmail.com>
2022-03-18 20:04:11 -0400

    Adding part2

---
+++ app/src/main/java/com/example/parstagram/fragments/FeedFragment.kt
@@ -0,0 +1,108 @@
+package com.example.parstagram.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.parstagram.MainActivity
+import com.example.parstagram.Post
+import com.example.parstagram.PostAdapter
+import com.example.parstagram.R
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
+
+open class FeedFragment : Fragment() {
+
+    lateinit var  postsRecyclerView: RecyclerView
+
+    lateinit var adapter: PostAdapter
+
+    var allPosts: MutableList<Post> = mutableListOf()
+
+    lateinit var swipeContainer: SwipeRefreshLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //this is where we set up our views and click listeners
+
+        postsRecyclerView = view.findViewById(R.id.postRecyclerView)
+
+        swipeContainer = view.findViewById(R.id.swipeContainer)
+
+        swipeContainer.setOnRefreshListener {
+            Log.i(TAG, "Refreshing timeline")
+            queryPosts()
+        }
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light)
+
+        //Steps to populate RecyclerView
+        //1. Create layout for each row in list (item_post.xml)
+        //2. Create data source for each row(this is the post class)
+        //3. Create adapter that will bridge data and row layout (PostAdapter)
+        //4. Set adapter on RecyclerView
+        adapter = PostAdapter(requireContext(), allPosts as ArrayList<Post>)
+        postsRecyclerView.adapter = adapter
+        //5. Set layout manager on RecyclerView
+
+        postsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        queryPosts()
+    }
+
+    //Query for all posts in our server
+    open fun queryPosts(){
+
+        //specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        // find all Post objects
+        query.include(Post.KEY_USER)
+        // Return the posts in descending order: ie newer posts will appear first
+        query.addDescendingOrder("createdAt")
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if(e != null) {
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    adapter.clear()
+                    if(posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG, "Post: " + post.getDescription() + " , username: " + post.getUser()?.username)
+                        }
+
+                        for(i in 0..19){
+                            allPosts.add(posts[i])
+                        }
+                        //allPosts.addAll(posts)
+                        adapter.notifyDataSetChanged()
+                        // Now we call setRefreshing(false) to signal refresh has finished
+                        swipeContainer.setRefreshing(false)
+                    }
+                }
+            }
+
+        })
+    }
+
+    companion object {
+        const val TAG = "FeedFragment"
+    }
+}
